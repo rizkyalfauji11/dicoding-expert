2d5eb41fd75ce9acc8465607102304473e58dfe3
Rizky Alfa Uji <rizkyalfauji11@gmail.com>
2021-08-18 14:13:11 +0700

    Initial Commit

---
+++ dicoding-expert/dicoding_expertApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicoding_expertApp.swift
+//  dicoding-expert
+//
+//  Created by Rizky Alfa Uji Gultom on 18/08/21.
+//
+
+import SwiftUI
+
+@main
+struct dicoding_expertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
